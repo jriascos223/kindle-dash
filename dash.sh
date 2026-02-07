9193813362e64c7ac9d291dedeea29f3af168cee
jorsc <josephriascos@gmail.com>
2026-02-07 12:09:00 -0500

    spacing and comment changes

--- dash.sh
+++ dash.sh
@@ -10,21 +10,28 @@ export LC_ALL="en_US.UTF-8"
 DASH_DIR="/mnt/us/kindle-dash"
 LOGFILE="${DASH_DIR}/dash.log"
 
-# Workaround vfat+fuse issues by running from tmpfs
+# NOTE: Stupid workaround to make sure the script we end up running is a *copy*,
+# living in a magical land that doesn't suffer from gross filesystem deficiencies.
+# Otherwise, the vfat+fuse mess means an OTA update will break the script on exit,
+# and potentially leave the user in a broken state, with the WM still paused...
+# Additionally, this is used by kindle-dash? to detect if the original script has
+# changed after an update (requiring a complete restart from the parent
+# launcher).
+# TLDR: moves execution to /var/tmp in case there's issues
 if [ "$(dirname "${0}")" != "/var/tmp" ]; then
-    cp -pf "${0}" /var/tmp/dash.sh
-    chmod 777 /var/tmp/dash.sh
-    exec /var/tmp/dash.sh "$@"
+	cp -pf "${0}" /var/tmp/dash.sh
+	chmod 777 /var/tmp/dash.sh
+	exec /var/tmp/dash.sh "$@"
 fi
 
 # Detect init system
 if [ -d /etc/upstart ]; then
-    INIT_TYPE="upstart"
-    # Source upstart logging functions
-    [ -f /etc/upstart/functions ] && . /etc/upstart/functions
+	INIT_TYPE="upstart"
+	# Source upstart logging functions
+	[ -f /etc/upstart/functions ] && . /etc/upstart/functions
 else
-    INIT_TYPE="sysv"
-    [ -f /etc/rc.d/functions ] && . /etc/rc.d/functions
+	INIT_TYPE="sysv"
+	[ -f /etc/rc.d/functions ] && . /etc/rc.d/functions
 fi
 
 # State tracking
@@ -38,160 +45,158 @@ VOLUMD_STOPPED="no"
 TOGGLED_SERVICES="stored webreader kfxreader kfxview todo tmd rcm archive scanner otav3 otaupd"
 
 log() {
-    msg="[$(date '+%Y-%m-%d %H:%M:%S')] $*"
-    echo "${msg}" >> "${LOGFILE}"
-    echo "${msg}"
+	msg="[$(date '+%Y-%m-%d %H:%M:%S')] $*"
+	echo "${msg}" >>"${LOGFILE}"
+	echo "${msg}"
 }
 
 # Find FBInk binary
 find_fbink() {
-    for path in \
-        /var/tmp/fbink \
-        "${DASH_DIR}/fbink" \
-        /mnt/us/koreader/fbink \
-        /mnt/us/libkh/bin/fbink \
-        /usr/bin/fbink
-    do
-        if [ -x "${path}" ]; then
-            echo "${path}"
-            return 0
-        fi
-    done
-    return 1
+	for path in \
+		/var/tmp/fbink \
+		"${DASH_DIR}/fbink" \
+		/mnt/us/koreader/fbink \
+		/mnt/us/libkh/bin/fbink \
+		/usr/bin/fbink; do
+		if [ -x "${path}" ]; then
+			echo "${path}"
+			return 0
+		fi
+	done
+	return 1
 }
 
 # Print message at bottom of screen
 eips_print_bottom() {
-    msg="${1}"
-    y_shift="${2:-0}"
+	msg="${1}"
+	y_shift="${2:-0}"
 
-    FBINK_BIN=$(find_fbink)
-    if [ -n "${FBINK_BIN}" ]; then
-        usleep 150000
-        ${FBINK_BIN} -qpm -y $((-4 - y_shift)) "${msg}"
-    fi
+	FBINK_BIN=$(find_fbink)
+	if [ -n "${FBINK_BIN}" ]; then
+		usleep 150000
+		${FBINK_BIN} -qpm -y $((-4 - y_shift)) "${msg}"
+	fi
 }
 
 # Find LuaJIT binary
 find_luajit() {
-    for path in \
-        "${DASH_DIR}/luajit" \
-        /mnt/us/koreader/luajit \
-        /usr/bin/luajit
-    do
-        if [ -x "${path}" ]; then
-            echo "${path}"
-            return 0
-        fi
-    done
-    return 1
+	for path in \
+		"${DASH_DIR}/luajit" \
+		/mnt/us/koreader/luajit \
+		/usr/bin/luajit; do
+		if [ -x "${path}" ]; then
+			echo "${path}"
+			return 0
+		fi
+	done
+	return 1
 }
 
 stop_pillow() {
-    if [ "${INIT_TYPE}" != "upstart" ]; then
-        return
-    fi
+	if [ "${INIT_TYPE}" != "upstart" ]; then
+		return
+	fi
 
-    log "Disabling pillow..."
-    lipc-set-prop com.lab126.pillow disableEnablePillow disable 2>/dev/null
-    PILLOW_DISABLED="yes"
+	log "Disabling pillow..."
+	lipc-set-prop com.lab126.pillow disableEnablePillow disable 2>/dev/null
+	PILLOW_DISABLED="yes"
 }
 
 start_pillow() {
-    if [ "${PILLOW_DISABLED}" = "yes" ]; then
-        log "Enabling pillow..."
-        lipc-set-prop com.lab126.pillow disableEnablePillow enable 2>/dev/null
-        lipc-set-prop com.lab126.appmgrd start app://com.lab126.booklet.home 2>/dev/null
-    fi
+	if [ "${PILLOW_DISABLED}" = "yes" ]; then
+		log "Enabling pillow..."
+		lipc-set-prop com.lab126.pillow disableEnablePillow enable 2>/dev/null
+		lipc-set-prop com.lab126.appmgrd start app://com.lab126.booklet.home 2>/dev/null
+	fi
 }
 
 stop_framework() {
-    log "Stopping framework..."
-    STOP_FRAMEWORK="yes"
+	log "Stopping framework..."
+	STOP_FRAMEWORK="yes"
 
-    if [ "${INIT_TYPE}" = "sysv" ]; then
-        /etc/init.d/framework stop
-    else
-        # Trap SIGTERM so we don't get killed
-        trap "" TERM
-        stop lab126_gui 2>/dev/null
-        usleep 1250000
-        trap - TERM
-    fi
+	if [ "${INIT_TYPE}" = "sysv" ]; then
+		/etc/init.d/framework stop
+	else
+		# Trap SIGTERM so we don't get killed
+		trap "" TERM
+		stop lab126_gui 2>/dev/null
+		usleep 1250000
+		trap - TERM
+	fi
 }
 
 start_framework() {
-    if [ "${STOP_FRAMEWORK}" = "yes" ]; then
-        log "Starting framework..."
-        if [ "${INIT_TYPE}" = "sysv" ]; then
-            /etc/init.d/framework start
-        else
-            start lab126_gui 2>/dev/null
-        fi
-    fi
+	if [ "${STOP_FRAMEWORK}" = "yes" ]; then
+		log "Starting framework..."
+		if [ "${INIT_TYPE}" = "sysv" ]; then
+			/etc/init.d/framework start
+		else
+			start lab126_gui 2>/dev/null
+		fi
+	fi
 }
 
 stop_services() {
-    # Stop background services to free RAM
-    if [ "${INIT_TYPE}" = "upstart" ]; then
-        for job in ${TOGGLED_SERVICES}; do
-            stop "${job}" 2>/dev/null
-        done
-    fi
+	# Stop background services to free RAM
+	if [ "${INIT_TYPE}" = "upstart" ]; then
+		for job in ${TOGGLED_SERVICES}; do
+			stop "${job}" 2>/dev/null
+		done
+	fi
 
-    # SIGSTOP volumd to prevent USBMS interference
-    if command -v volumd >/dev/null 2>&1 || [ -e /etc/init.d/volumd ] || [ -e /etc/upstart/volumd.conf ]; then
-        log "Stopping volumd..."
-        killall -STOP volumd 2>/dev/null
-        VOLUMD_STOPPED="yes"
-    fi
+	# SIGSTOP volumd to prevent USBMS interference
+	if command -v volumd >/dev/null 2>&1 || [ -e /etc/init.d/volumd ] || [ -e /etc/upstart/volumd.conf ]; then
+		log "Stopping volumd..."
+		killall -STOP volumd 2>/dev/null
+		VOLUMD_STOPPED="yes"
+	fi
 
-    # SIGSTOP cvm on sysv
-    if [ "${INIT_TYPE}" = "sysv" ]; then
-        log "Stopping cvm..."
-        killall -STOP cvm 2>/dev/null
-        CVM_STOPPED="yes"
-    fi
+	# SIGSTOP cvm on sysv
+	if [ "${INIT_TYPE}" = "sysv" ]; then
+		log "Stopping cvm..."
+		killall -STOP cvm 2>/dev/null
+		CVM_STOPPED="yes"
+	fi
 
-    # SIGSTOP awesome on upstart
-    if [ "${INIT_TYPE}" = "upstart" ]; then
-        log "Stopping awesome..."
-        killall -STOP awesome 2>/dev/null
-        AWESOME_STOPPED="yes"
-    fi
+	# SIGSTOP awesome on upstart
+	if [ "${INIT_TYPE}" = "upstart" ]; then
+		log "Stopping awesome..."
+		killall -STOP awesome 2>/dev/null
+		AWESOME_STOPPED="yes"
+	fi
 }
 
 resume_services() {
-    if [ "${AWESOME_STOPPED}" = "yes" ]; then
-        log "Resuming awesome..."
-        killall -CONT awesome 2>/dev/null
-    fi
+	if [ "${AWESOME_STOPPED}" = "yes" ]; then
+		log "Resuming awesome..."
+		killall -CONT awesome 2>/dev/null
+	fi
 
-    if [ "${CVM_STOPPED}" = "yes" ]; then
-        log "Resuming cvm..."
-        killall -CONT cvm 2>/dev/null
-    fi
+	if [ "${CVM_STOPPED}" = "yes" ]; then
+		log "Resuming cvm..."
+		killall -CONT cvm 2>/dev/null
+	fi
 
-    if [ "${VOLUMD_STOPPED}" = "yes" ]; then
-        log "Resuming volumd..."
-        killall -CONT volumd 2>/dev/null
-    fi
+	if [ "${VOLUMD_STOPPED}" = "yes" ]; then
+		log "Resuming volumd..."
+		killall -CONT volumd 2>/dev/null
+	fi
 
-    # Restart toggled services
-    if [ "${INIT_TYPE}" = "upstart" ]; then
-        for job in ${TOGGLED_SERVICES}; do
-            start "${job}" 2>/dev/null
-        done
-    fi
+	# Restart toggled services
+	if [ "${INIT_TYPE}" = "upstart" ]; then
+		for job in ${TOGGLED_SERVICES}; do
+			start "${job}" 2>/dev/null
+		done
+	fi
 }
 
 cleanup() {
-    log "Cleaning up..."
-    resume_services
-    start_pillow
-    start_framework
-    log "=== Kindle-Dash finished ==="
-    exit 0
+	log "Cleaning up..."
+	resume_services
+	start_pillow
+	start_framework
+	log "=== Kindle-Dash finished ==="
+	exit 0
 }
 
 # Trap signals
@@ -204,16 +209,16 @@ log "Init type: ${INIT_TYPE}"
 
 # Copy FBInk to tmpfs if available
 if [ -x "${DASH_DIR}/fbink" ]; then
-    cp -pf "${DASH_DIR}/fbink" /var/tmp/fbink
-    chmod 777 /var/tmp/fbink
+	cp -pf "${DASH_DIR}/fbink" /var/tmp/fbink
+	chmod 777 /var/tmp/fbink
 fi
 
 # Find LuaJIT
 LUAJIT=$(find_luajit)
 if [ -z "${LUAJIT}" ]; then
-    log "Error: LuaJIT not found"
-    eips_print_bottom "Error: LuaJIT not found" 0
-    exit 1
+	log "Error: LuaJIT not found"
+	eips_print_bottom "Error: LuaJIT not found" 0
+	exit 1
 fi
 log "Using LuaJIT: ${LUAJIT}"
 
@@ -221,8 +226,8 @@ log "Using LuaJIT: ${LUAJIT}"
 export LD_LIBRARY_PATH="${DASH_DIR}/libs:/mnt/us/koreader/libs:${LD_LIBRARY_PATH}"
 
 # Unlock input devices
-[ -e /proc/keypad ] && echo unlock > /proc/keypad
-[ -e /proc/fiveway ] && echo unlock > /proc/fiveway
+[ -e /proc/keypad ] && echo unlock >/proc/keypad
+[ -e /proc/fiveway ] && echo unlock >/proc/fiveway
 
 # Stop UI components
 stop_pillow
@@ -233,7 +238,7 @@ eips_print_bottom "Starting Kindle-Dash..." 0
 # Run the dash
 cd "${DASH_DIR}" || exit 1
 log "Running dash.lua..."
-"${LUAJIT}" dash.lua >> "${LOGFILE}" 2>&1
+"${LUAJIT}" dash.lua >>"${LOGFILE}" 2>&1
 RETURN_VALUE=$?
 log "dash.lua exited with code ${RETURN_VALUE}"
 
